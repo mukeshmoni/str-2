cde9450fda6fb23d32c79598be72598b65107292
MUKESH BABU S <108213299+mukeshmoni@users.noreply.github.com>
2024-09-26 12:32:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-API_ID = int(getenv("API_ID", "19197570"))
-API_HASH = getenv("API_HASH", "0401e837796d193ec9ae6e17cb8cfbf8")
+API_ID = int(getenv("API_ID", "15692563"))
+API_HASH = getenv("API_HASH", "03ab15637e799898d233250c731cd114")
 
 BOT_TOKEN = getenv("BOT_TOKEN", "5873474775:AAF3oD8pGtbHt5dJp2QDZO6qt0swoaAdId8")
 OWNER_ID = int(getenv("OWNER_ID", "6798074818"))
